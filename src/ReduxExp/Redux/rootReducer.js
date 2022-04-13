d5873b2b0c34e2cb06435f37c0d3654899fa50d3
nndyalartr <nandyalartr@gmail.com>
2022-04-13 15:39:26 +0530

    Task

--- src/ReduxExp/Redux/rootReducer.js
+++
@@ -1,5 +0,0 @@
-import { combineReducers } from "redux";
-import { MessageReducer } from "./Message/Message.reducer";
-import { Counterreducer } from "./Message/Counter.reducer";
-let rootReducer = combineReducers({counter:Counterreducer, msg:MessageReducer})
-export {rootReducer} 
